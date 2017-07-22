8e275c71610cab7b29805f9a2ddd7cc143b8f58a
tedcallon <tedcallon@gmail.com>
2017-07-22 14:36:16 -0400

    Fixed margins and legend

--- plot4.R
+++ plot4.R
@@ -12,7 +12,7 @@ use$Time <- strptime(use$Time, format = "%Y-%m-%d %H:%M:%S")
 ##plot 4
 
 png(filename = "plot4.png", width = 480, height = 480)
-par(mfrow = c(2,2), mar = c(5,4,4,4))
+par(mfrow = c(2,2), mar = c(5,4,4,2))
 #plot 1
 plot(y = as.numeric(use$Global_active_power), x = use$Time, type = "l", xlab = "",
      ylab = "Global Active Power")
@@ -26,7 +26,8 @@ plot(y = use$Sub_metering_1, x = use$Time, type = "l", xlab = "",
      ylab = "Energy sub metering")
 lines(y = use$Sub_metering_2, x = use$Time, col = "red")
 lines(y = use$Sub_metering_3, x = use$Time, col = "blue")
-legend("topright", legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), col = c("black","red","blue"), lwd = 1, lty = 1)
+legend("topright", legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), col = c("black","red","blue"), 
+       lwd = 1, lty = 1, bty = "n")
 
 #plot 4
 plot(y = use$Global_reactive_power, x = use$Time, type = "l", xlab = "datetime",
